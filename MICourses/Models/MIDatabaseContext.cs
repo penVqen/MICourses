adad0a6a877323194cb57c3649c90eb5599a8cea
awaayy <phantomy.design@gmail.com>
2024-11-27 02:32:28 +0300

    Создание локального подключения к базе данных

--- MICourses/Models/MIDatabaseContext.cs
+++ MICourses/Models/MIDatabaseContext.cs
@@ -23,7 +23,7 @@ public partial class MIDatabaseContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-JVTTJKA\\SQLEXPRESS;Initial Catalog=MIDatabase;Integrated Security=True;Encrypt=False");
+        => optionsBuilder.UseSqlServer("Data Source=26.134.40.253,1433;Initial Catalog=MIDatabase;Persist Security Info=True;User ID=maxim;Password=dlagita; TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -40,7 +40,7 @@ public partial class MIDatabaseContext : DbContext
                 .HasMaxLength(50);
             entity.Property(e => e.Name)
                 .IsRequired()
-                .HasMaxLength(50);
+                .HasMaxLength(150);
             entity.Property(e => e.StudyPeriod)
                 .IsRequired()
                 .HasMaxLength(50);
